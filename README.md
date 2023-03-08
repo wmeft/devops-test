201c478772508a751edf1d4b49c0a41101895aa5
Bilal Djaghout <b.djaghout@gmail.com>
2023-03-08 22:59:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DevOps Test microservice
+
+This repo contains a microservice application (Blog application).
+
+_Please make sure to right your docs here._
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+const element = document.getElementById("root");
+const root = createRoot(element);
+root.render(<App />);
