3660974d168cd2ab6dc64f082cb3ddbbc8960496
wmeft <65124955+wmeft@users.noreply.github.com>
2023-03-26 09:43:38 +0100

    Update query Url with query kubernetes svc

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -6,7 +6,7 @@ import CommentList from "./CommentList";
 const PostList = () => {
   const [posts, setPosts] = useState({});
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://svc-query/posts");
     console.log(res.data);
     setPosts(res.data);
   };
